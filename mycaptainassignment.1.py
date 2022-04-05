4aa333221c190d5258b7cb1c95b0bc8a9fe9a671
saniyamasood24 <103058531+saniyamasood24@users.noreply.github.com>
2022-04-05 22:37:03 +0530

    Add files via upload

---
+++ mycaptainassignment.1.py
@@ -0,0 +1,3 @@
+from math import pi
+r=float(input("input the radius of the circle:"))
+print("area of the circle with radius 1.1 is:"+ str(pi*r**2))
---
+++ mycaptainassignment.2.py
@@ -0,0 +1,10 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/HP/Desktop/my captain assingments/mycaptainassignment.1.py =
+input the radius of the circle:1.1
+area of the circle with radius 1.1 is:3.8013271108436504
+
+= RESTART: C:/Users/HP/Desktop/my captain assingments/mycaptainassignment.2.py =
+Input the Filename: abc.py
+The extension of the file is : 'py'
