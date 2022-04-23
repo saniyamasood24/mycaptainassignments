347d9b5fd40ed129f48ea8d34fb2116552748dd9
saniyamasood24 <103058531+saniyamasood24@users.noreply.github.com>
2022-04-23 23:30:42 +0530

    Add files via upload

---
+++ fileextention.py
@@ -0,0 +1,6 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/HP/Desktop/my captain assingments/fileextention.py =====
+Enter the filename:ass.py
+The extention of the file is: py
