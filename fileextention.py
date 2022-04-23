247ed138309f119de752f426681233cccb3afd0e
saniyamasood24 <103058531+saniyamasood24@users.noreply.github.com>
2022-04-23 23:36:44 +0530

    Add files via upload

--- fileextention.py
+++ fileextention.py
@@ -1,6 +1,3 @@
-Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-
-===== RESTART: C:/Users/HP/Desktop/my captain assingments/fileextention.py =====
-Enter the filename:ass.py
-The extention of the file is: py
+filename=input("Enter the filename:")
+ext=filename.split(".")
+print("The extention of the file is:",ext[-1])
